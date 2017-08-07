36a9c4dc70913e4e395c6955ecc69b897e4d47a5
Ted <ted@theo>
2017-08-07 14:37:17 +1200

    Add new tests for candidate not existing

--- countmeup_test.go
+++ countmeup_test.go
@@ -1,9 +1,8 @@
-package cmu
+package main
 
 import (
 	"testing"
 )
-
 
 //Desired: Max three votes. should only have three votes for user
 func TestUserMoreThanThreeVotes(t *testing.T) {
@@ -39,27 +38,34 @@ func TestNonExistingUser(t *testing.T) {
 	}
 }
 
+func TestNonExistingCandidate(t *testing.T) {
+	if(candidateTotals[6] != 0) {
+		t.Fail()
+	}
+}
+
 //Desired: 1 should have 50% of the votes, 6 total
 // 2 should have 25% of the votes, 3 total
 // 3 should have 35% of the votes, 3 total
 func TestCandidateVoteTallies(t *testing.T) {
-	insertVote("1","1")
-	insertVote("1","1")
-	insertVote("1","1")
-	insertVote("1","1")
-	insertVote("1","1")
-	insertVote("2","1")
-	insertVote("2","1")
-	insertVote("2","1")
-	insertVote("2","1")
-	insertVote("3","2")
-	insertVote("3","2")
-	insertVote("3","2")
-	insertVote("4","3")
-	insertVote("4","3")
-	insertVote("4","3")
+	insertVote("5","6")
+	insertVote("5","6")
+	insertVote("5","6")
+	insertVote("5","6") //should void
+	insertVote("5","6") //should void
+	insertVote("6","6")
+	insertVote("6","6")
+	insertVote("6","6")
+	insertVote("6","6") //should void
+	insertVote("7","7")
+	insertVote("7","7")
+	insertVote("7","7")
+	insertVote("8","8")
+	insertVote("8","8")
+	insertVote("8","8")
+	insertVote("8","8") //should void
 	
-	if(candidateTotals[1] != 6 || candidateTotals[2] != 3 || candidateTotals[3] != 3) {
+	if(candidateTotals[6] != 6 || candidateTotals[7] != 3 || candidateTotals[8] != 3) {
 		t.Fail()
 	}
 }
